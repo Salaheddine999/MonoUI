938842ee87168332482f0e9afa9fb2f07d2203a3
Salah eddine <abw.salaheddine@gmail.com>
2025-09-11 23:05:17 +0100

    fixes

--- src/components/ui/liquid-glass-input.tsx
+++ src/components/ui/liquid-glass-input.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { cn } from "@/lib/utils";
 
 interface LiquidGlassInputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "size"> {
   size?: "sm" | "md" | "lg";
 }
 
